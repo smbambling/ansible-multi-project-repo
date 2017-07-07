91eab812425967abdc1ee587f96cd39beb36c1dc
Steven Bambling <smbambling@gmail.com>
2017-07-07 13:50:21 -0400

    adding more stuff

---
+++ corporate_roles/base/spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'serverspec'
+require 'net/ssh'
+
+host = ENV['TARGET_HOST']
+ssh_config_files = ['./.vagrant/ssh-config'] + Net::SSH::Config.default_files
+options = Net::SSH::Config.for(host, ssh_config_files)
+options[:user] ||= 'vagrant'
+options[:keys].push("#{Dir.home}/.vagrant.d/insecure_private_key")
+
+set :backend, :ssh
+set :host, host
+set :ssh_options, options
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = 'documentation'
+  config.output_stream = File.open("spec/reports/#{host}-results.xml", 'w')
+  config.formatter = 'RspecJunitFormatter'
+end
